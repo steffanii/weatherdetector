a01edd9c3249dfb4367ff0576d3a32e7a321938c
steffanii <stephanienwosumadueke@gmail.com>
2026-05-05 21:47:53 -0100

    fix vercel config

--- weatherdetector/settings.py
+++ weatherdetector/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'weather'
+    'weather',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
